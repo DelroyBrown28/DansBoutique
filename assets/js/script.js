95b146a8850198d5d9eacee342e2767a1ce80d9b
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-29 20:02:28 +0100

    updated hair section toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
 
-    $('.hair_service').click( function() {
-        $('.hair_section').fadeIn();
+    $('.hair_service').click(function () {
+        $('.hair_section').fadeIn("fast", function () {
+            $('.hair_section').animate({
+                left: "0"
+            })
+        });
     })
 
-    $('.back_button').click( function() {
-        $('.hair_section').fadeOut();
+    $('.back_button').click(function () {
+        $('.hair_section').animate({
+            left: '100%'
+        }, function () {
+            $('.hair_section').fadeOut();
+        })
     })
+
 });
