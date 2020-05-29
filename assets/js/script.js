043e3b355d6004338e30025fe40d9ae561602fbb
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-29 17:04:20 +0100

    added function to nxt page (hair section)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
 
+    $('.hair_service').click( function() {
+        $('.hair_section').fadeIn();
+    })
 
+    $('.back_button').click( function() {
+        $('.hair_section').fadeOut();
+    })
 });
