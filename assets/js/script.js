0b1f7d3a09fb8e44cf238f88943aef17ecceaae2
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-06-02 00:54:39 +0100

    added men or women hair style choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
 
     $('.hair_service').click(function () {
-        $('.hair_section').fadeIn("fast", function () {
-            $('.hair_section').animate({
-                left: "0"
-            })
-        });
-    })
-
-    $('.back_button').click(function () {
-        $('.hair_section').animate({
-            left: '100%'
+        $(this).animate({
+            width: '100%'
         }, function () {
-            $('.hair_section').fadeOut();
+            $(this).animate({
+                height: '100%'
+            }, function() {
+                $('.men_women_fadein').fadeIn();
+            })
         })
     })
 
 });
+
